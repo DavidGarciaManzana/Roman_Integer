bd6372e177d6c78c87de0cf6160c61beea9aafda
David Garcia <Davidgarciama@outlook.com>
2023-02-17 17:11:10 -0600

    Refactoring some code

--- src/main.ts
+++ src/main.ts
@@ -1,146 +1,89 @@
-// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
-
-// Symbol       Value
-// I             1
-// V             5
-// X             10
-// L             50
-// C             100
-// D             500
-// M             1000
-// For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
-
-// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
-
-// I can be placed before V (5) and X (10) to make 4 and 9. 
-// X can be placed before L (50) and C (100) to make 40 and 90. 
-// C can be placed before D (500) and M (1000) to make 400 and 900.
-// Given a roman numeral, convert it to an integer.
-
-
-// Constraints:
-
-// 1 <= s.length <= 15
-// s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
-// It is guaranteed that s is a valid roman numeral in the range [1, 3999].
-
-
-
-
 let romanToInt = (s: string): number => {
-    const hashTable: { [key: string]: number } = {};
     let result: number = 0;
-    for (let i: number = 0; i < s.length; i++) {
-        if (i == 0) {
-            hashTable[s[i]] = 0
-            // console.log(hashTable)
+    const hashTable: { [key: string]: number } = {};
+    const addToTheHash = (i:number) => {
+        if (!hashTable[s[i]]) {
+            hashTable[s[i]] = 1
+        } else {
+            hashTable[s[i]] += 1
         }
+    }
+
+    for (let i: number = 0; i < s.length; i++) {
 
         switch (s[i]) {
 
             case "I": {
-                if (!hashTable[s[i]]) {
-                    hashTable[s[i]] = 1
-                } else {
-                    hashTable[s[i]] += 1
-                }
+                addToTheHash(i)
                 break;
             }
+
             case "V": {
                 if (s[i - 1] == "I") {
                     result += 4
                     delete hashTable[s[i - 1]];
                 } else {
-                    if (!hashTable[s[i]]) {
-                        hashTable[s[i]] = 1
-                    } else {
-                        hashTable[s[i]] += 1
-                    }
+                    addToTheHash(i)
                 }
-                // console.log(hashTable)
                 break;
             }
+
             case "X": {
                 if (s[i - 1] == "I") {
                     result += 9
                     delete hashTable[s[i - 1]];
                 } else {
-                    if (!hashTable[s[i]]) {
-                        hashTable[s[i]] = 1
-                    } else {
-                        hashTable[s[i]] += 1
-                    }
+                    addToTheHash(i)
                 }
                 break;
             }
+
             case "L": {
                 if (s[i - 1] == "X") {
                     result += 40
                     delete hashTable[s[i - 1]];
                 } else {
-                    if (!hashTable[s[i]]) {
-                        hashTable[s[i]] = 1
-                    } else {
-                        hashTable[s[i]] += 1
-                    }
+                    addToTheHash(i)
                 }
                 break;
             }
+
             case "C": {
                 if (s[i - 1] == "X") {
                     result += 90
                     delete hashTable[s[i - 1]];
                 } else {
-                    if (!hashTable[s[i]]) {
-                        hashTable[s[i]] = 1
-                    } else {
-                        hashTable[s[i]] += 1
-                    }
-
-                    // console.log(hashTable)
+                    addToTheHash(i)
                 }
                 break;
             }
+
             case "D": {
                 if (s[i - 1] == "C") {
                     result += 400
                     delete hashTable[s[i - 1]];
                 } else {
-                    if (!hashTable[s[i]]) {
-                        hashTable[s[i]] = 1
-                    } else {
-                        hashTable[s[i]] += 1
-                    }
-
+                    addToTheHash(i)
                 }
                 break;
             }
+
             case "M": {
                 if (s[i - 1] == "C") {
                     result += 900
                     delete hashTable[s[i - 1]];
                 } else {
-                    if (!hashTable[s[i]]) {
-                        hashTable[s[i]] = 1
-                    } else {
-                        hashTable[s[i]] += 1
-                    }
+                    addToTheHash(i)
                 }
                 break;
             }
-
         }
-
     }
-    // console.log(hashTable,result)
 
     if (hashTable["I"]) {
         result += hashTable["I"]
     }
-
-
     if (hashTable["V"]) {
-        // console.log("aaaa")
         result += (hashTable["V"]) * 5
     }
 
@@ -148,7 +91,6 @@ let romanToInt = (s: string): number => {
         result += (hashTable["X"]) * 10
     }
     if (hashTable["L"]) {
-        // console.log((hashTable["L"] ) * 50)
         result += (hashTable["L"]) * 50
     }
     if (hashTable["C"]) {
@@ -172,6 +114,5 @@ let romanToInt = (s: string): number => {
 // console.log(romanToInt("CMXCIX")) //999
 // console.log(romanToInt("MMMCCCXCIX")) //3399
 // console.log(romanToInt("XXIX")) //29
-
-console.log(romanToInt("LVIII")) //58
+// console.log(romanToInt("LVIII")) //58
 // console.log(romanToInt("III")) //3
